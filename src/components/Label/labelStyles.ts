52581e3644af29404c9fff1ad539e44c7bd92021
Igor Zupunski <ipatheword@live.com>
2023-09-19 15:38:48 +0200

    changes to styling

--- src/components/Label/labelStyles.ts
+++ src/components/Label/labelStyles.ts
@@ -1,14 +1,16 @@
-import "../../styles/colors.scss";
 import styled from 'styled-components';
 
 export const StyledFormLabel = styled.label`
-    color: red;
+    font-size: 16px;
+    color: #333;
+    font-weight: bold;
+    display: block;
 `;
 
 export const StyledDefaultLabel = styled.label`
     font-size: 14px;
-    color: #000; /* black */
-    background-color: #fff; /* white */
+    color: #000; 
+    background-color: #fff; 
     padding: 8px;
-    border: 1px solid #ccc; /* light gray */
+    border: 1px solid #ccc; 
 `;
