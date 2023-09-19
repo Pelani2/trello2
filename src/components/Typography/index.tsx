224109f5092e39a63ef6611e65e5f75c5d8d3676
Igor Zupunski <ipatheword@live.com>
2023-09-19 14:44:39 +0200

    update to styles

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import styled from 'styled-components';
-import "./typography-styles.scss";
+import { StyledPrimaryTitle, StyledDefault } from "./typographyStyles";
 
 const variantClassMap:  Record<string, string> = {
     "primary-title": "primary-title",
@@ -10,16 +9,6 @@ interface TypographyProps {
     variant: keyof typeof variantClassMap;
     children: React.ReactNode;
 }
-
-const StyledPrimaryTitle = styled.div`
-    font-size: 24px;
-    color: #007bff;
-`;
-
-const StyledDefault = styled.div`
-    font-size: 16px;
-    color: #333;
-`;
 
 const Typography: React.FC<TypographyProps> = ({ variant, children }) => {
     let StyledTypography;
---
+++ src/components/Typography/typographyStyles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledPrimaryTitle = styled.div`
+    font-size: 24px;
+    color: #007bff;
+`;
+
+export const StyledDefault = styled.div`
+    font-size: 16px;
+    color: #333;
+`;
