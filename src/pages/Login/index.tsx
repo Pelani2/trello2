5661b097dbe9fa1b4c8de50afb43713aa0f3ace2
Igor Zupunski <ipatheword@live.com>
2023-09-21 13:01:27 +0200

    implemented schema and onsubmit

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -12,3 +12,21 @@ const validationSchema = yup.object().shape({
     email: yup.string().required("Email is required").email("Invalid email format"),
     password: yup.string().required("Password is required"),
 });
+
+const Login: React.FC = () => {
+    const {
+        handleSubmit, 
+        control,
+        formState: { errors },
+    } = useForm<FormData>({
+        resolver: yupResolver(validationSchema),
+    });
+
+    const onSubmit = (data: FormData) => {
+        console.log(`Form submitted with the following data: 
+        \n\nEmail: ${data.email}
+        \nPassword: ${data.password}`);
+    };
+};
+
+export default Login;
