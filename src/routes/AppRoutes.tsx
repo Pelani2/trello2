12b36a51ae1c4ec88b028b030bdc45ff0f1a9154
Igor Zupunski <ipatheword@live.com>
2023-09-21 13:02:21 +0200

    made route

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Signup from "../pages/Signup";
+import Login from "../pages/Login";
 
 const AppRoutes = [
     {
-        path: "/",
+        path: "/signup",
         element: <Signup />
     },
+    {
+        path: "/",
+        element: <Login />
+    }
 ];
 
 export default AppRoutes;
