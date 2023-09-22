31893f1507b6ec1a58254d876e470674df00dff5
Igor Zupunski <ipatheword@live.com>
2023-09-22 13:43:06 +0200

    started pass reset page

--- src/pages/ResetPassword/index.ts
+++ src/pages/ResetPassword/index.ts
@@ -1,1 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useForm, Controller } from 'react-hook-form';
+import * as yup from 'yup';
+import { passwordResetStart, passwordResetFailure, passwordResetSuccess } from "../../store/actions/passwordResetSlice";
+import axios from "axios";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface FormInputs {
+    email: string;
+}
+
+const validationSchema = yup.object().shape({
+    email: yup.string().required("Email is required").email("Invalid email format"),
+});
+
+const ResetPassword: React.FC = () => {
+    const dispatch = useDispatch();
+    
+    const {
+        handleSubmit, 
+        control, 
+        formState: { errors },
+    } = useForm<FormInputs>({
+        resolver: yupResolver(validationSchema),
+    });
+};
