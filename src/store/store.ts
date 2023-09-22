f3f682b47d46fd429098a6336f69d6d9ebe64e69
Igor Zupunski <ipatheword@live.com>
2023-09-22 13:33:12 +0200

    updated store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import passwordValidationReducer from './actions/passwordValidationSlice';
 import rememberPasswordReducer from './actions/rememberPasswordSlice';
+import passwordResetReducer from './actions/passwordResetSlice';
 
 const store = configureStore({
     reducer: {
         passwordValidation: passwordValidationReducer,
         rememberPassword: rememberPasswordReducer,
+        passwordReset: passwordResetReducer,
     },
 });
 
