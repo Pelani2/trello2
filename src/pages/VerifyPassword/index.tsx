b8221ccf7d1ae06f864e4e5a4358b01d6f38e246
Igor Zupunski <ipatheword@live.com>
2023-09-25 11:52:13 +0200

    starting verify pass page

--- src/pages/VerifyPassword/index.tsx
+++ src/pages/VerifyPassword/index.tsx
@@ -1,4 +1,21 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useForm, Controller } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import Typography from "../../components/Typography";
+import Label from "../../components/Label";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+interface FormInputs {
+    resetCode: string;
+}
+
+const validationSchema = yup.object().shape({
+    resetCode: yup.string().required("Reset code is required"),
+});
 
 const VerifyPassword: React.FC = () => {
     return (
