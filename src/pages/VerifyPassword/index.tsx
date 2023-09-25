5d6ff82b268e0acee10729cedee55bbdf9129f06
Igor Zupunski <ipatheword@live.com>
2023-09-25 12:02:30 +0200

    finished layout

--- src/pages/VerifyPassword/index.tsx
+++ src/pages/VerifyPassword/index.tsx
@@ -8,6 +8,7 @@ import Typography from "../../components/Typography";
 import Label from "../../components/Label";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
+import { StyledForm, StyledFormGroup, StyledButtonWrapper } from "../../styles/loginSignupStyles";
 
 interface FormInputs {
     resetCode: string;
@@ -43,9 +44,48 @@ const VerifyPassword: React.FC = () => {
     };
 
     return (
-        <div>
-            Verify
-        </div>
+        <StyledForm onSubmit={handleSubmit(onSubmit)}>
+            <Typography variant="primary-title">
+                Verify your email
+            </Typography>
+            <Typography variant="sub-title">
+                {"Please enter the reset code set to your mail"}
+            </Typography>
+            <StyledFormGroup>
+                <Label 
+                    htmlFor="resetCode"
+                    variant="form-label"
+                >
+                    Reset Code:
+                </Label>
+                <Controller 
+                    name="resetCode"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                        <Input 
+                            {...field}
+                            placeholder="Enter your reset code"
+                            variant="form-input"
+                            type="text"
+                        />
+                    )}
+                />
+            </StyledFormGroup>
+            {errors.resetCode && (
+                <Typography variant="error-message">
+                    {errors.resetCode.message}
+                </Typography>
+            )}
+            <StyledButtonWrapper>
+                <Button
+                    type="submit"
+                    variant="submit-button"
+                >
+                    Verify Email
+                </Button>
+            </StyledButtonWrapper>
+        </StyledForm>
     );
 };
 
