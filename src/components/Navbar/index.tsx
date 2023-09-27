f1ddd7b7255f71c27f1fc88a76bc8e40376b5c8d
Igor Zupunski <ipatheword@live.com>
2023-09-27 11:47:29 +0200

    implemented profile icon

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,6 +6,8 @@ import TrelloIcon from "../../assets/images/icons/TrelloLogo.png";
 import StatsIcon from "../../assets/images/icons/TrelloStatIcon.png";
 import SearchBar from "../SearchBar";
 import NavbarIcon from "../NavbarIcon";
+import ProfileIcon from "../ProfileIcon";
+import FaceIcon from "../../assets/images/icons/ProfileIcon.png";
 
 const Navbar: React.FC = () => {
     const handleSearch = (searchTerm: string) => {
@@ -40,11 +42,14 @@ const Navbar: React.FC = () => {
             </StyledNavContentWrapper>
 
             <StyledNavContentWrapper>
-
                 <NavbarIcon variant="plus" />
                 <NavbarIcon variant="exclamation" />
                 <NavbarIcon variant="bell" />
-                
+                <ProfileIcon 
+                    src={FaceIcon}
+                    alt="profile icon"
+                    variant="navbar-profile-icon"
+                />
             </StyledNavContentWrapper>
         </StyledNavbarContainer>
     );
--- src/components/ProfileIcon/ProfileIconStyles.ts
+++ src/components/ProfileIcon/ProfileIconStyles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledNavbarProfileIcon = styled.img`
-    width: 50px;
-    height: 50px;
+    width: 60px;
+    height: 60px;
     border-radius: 50%;
 `;
