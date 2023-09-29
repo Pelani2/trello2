4c494ab9a367069d771163b2e31fed602bc3476e
Igor Zupunski <ipatheword@live.com>
2023-09-29 08:13:39 +0200

    brackets layout done

--- src/components/Brackets/BracketsStyles.ts
+++ src/components/Brackets/BracketsStyles.ts
@@ -2,13 +2,34 @@ import styled from "styled-components";
 
 export const StyledBracketsContainer = styled.div`
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
+    padding: 15px;
 `;
 
 export const StyledBracketsIcons = styled.div`
-    border: 1px solid black;
     display: flex;
     justify-content: center;
     align-items: center;
+    gap: 20px;
+`;
+
+export const StyledPublicWrapper = styled.div`
+    display: flex;
+    gap: 5px;
+    align-items: center;
+`;
+
+export const StyledBracketsAndIconsWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 100px;
+`;
+
+export const StyledIconsAndMenuWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 300px;
 `;
--- src/components/Brackets/index.tsx
+++ src/components/Brackets/index.tsx
@@ -2,36 +2,54 @@ import React from "react";
 import Typography from "../Typography";
 import HeartIcon from "../HeartIcon";
 import { StyledVerticalBreak } from "../Navbar/navbarStyles";
-import { StyledBracketsContainer, StyledBracketsIcons } from "./BracketsStyles";
+import { StyledBracketsContainer, StyledBracketsIcons, StyledPublicWrapper, StyledBracketsAndIconsWrapper, StyledIconsAndMenuWrapper } from "./BracketsStyles";
 import Icon from "../Icon";
 import GlobeIcon from "../../assets/images/icons/SomeShapeIcon.png";
 import HardIcon from "../../assets/images/icons/HardShapeIcon.png";
+import UsersIcons from "../../assets/images/icons/Users.png";
 
 const Brackets: React.FC = () => {
     return (
         <StyledBracketsContainer>
-            <Typography variant="navbar-text">
-                Brackets
-            </Typography>
+            <StyledBracketsAndIconsWrapper>
+                <Typography variant="navbar-text">
+                    Brackets
+                </Typography>
 
-            <StyledBracketsIcons>
-                <HeartIcon />
-                <StyledVerticalBreak />
+                <StyledBracketsIcons>
+                    <HeartIcon />
+                    <StyledVerticalBreak />
+
+                    <StyledPublicWrapper>
+                        <Icon 
+                            variant="brackets-icon"
+                            alt="globe icon"
+                            src={GlobeIcon}
+                        />
+                        <Typography variant="navbar-text">
+                            Public
+                        </Typography>
+                    </StyledPublicWrapper>
+                    
+                    <StyledVerticalBreak />
+                    <Icon 
+                        variant="brackets-icon"
+                        alt="hard icon"
+                        src={HardIcon}
+                    />
+                </StyledBracketsIcons>
+            </StyledBracketsAndIconsWrapper>
+            
+            <StyledIconsAndMenuWrapper>
                 <Icon 
                     variant="brackets-icon"
-                    alt="globe icon"
-                    src={GlobeIcon}
+                    alt="users icons"
+                    src={UsersIcons}
                 />
                 <Typography variant="navbar-text">
-                    Public
+                    Menu
                 </Typography>
-                <StyledVerticalBreak />
-                <Icon 
-                    variant="brackets-icon"
-                    alt="hard icon"
-                    src={HardIcon}
-                />
-            </StyledBracketsIcons>
+            </StyledIconsAndMenuWrapper>
         </StyledBracketsContainer>
     );
 };
