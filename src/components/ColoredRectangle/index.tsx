b05d9b2af669ad8bf924183e3f870c272457fdbf
Igor Zupunski <ipatheword@live.com>
2023-10-01 12:26:01 +0200

    update

--- src/components/ColoredRectangle/index.tsx
+++ src/components/ColoredRectangle/index.tsx
@@ -9,8 +9,9 @@ const StyledRectangle = styled.div<ColoredRectangleProps>`
     width: 60px;
     height: 8px;
     border-radius: 8px;
+    box-shadow: 0 5px 5px rgba(0, 0, 0, 0.7);
     background-color: ${(props) => {
-
+      
     switch (props.variant) {
       case "yellow":
         return "#F8BD1C";
