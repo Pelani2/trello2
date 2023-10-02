687ed4d54ae8ee37f9953907b0e1abe2e8f1b1e7
Igor Zupunski <ipatheword@live.com>
2023-10-02 14:39:21 +0200

    code improvements

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,54 +1,14 @@
 import React from "react";
 import Card from "../Card";
-import UserIcon5 from "../../assets/images/icons/users/user5.png";
-import OvalPlusIcon from "../../assets/images/icons/users/OvalPlusIcon.png";
-import UserIcon6 from "../../assets/images/icons/users/user6.png";
-import UserIcon7 from "../../assets/images/icons/users/user7.png";
-import CardImage1 from "../../assets/images/CardImage1.png";
-import UserIcon from "../../assets/images/icons/users/user.png";
-import UserIcon8 from "../../assets/images/icons/users/user8.png";
-import UserIcon9 from "../../assets/images/icons/users/user9.png";
+
 import { StyledCardsWrapper } from "./CardsStyles";
 import { CardProps } from "../Card";
 
+interface CardsProps {
+    cardsData: CardProps[];
+}
 
-const cardsData: CardProps[] = [
-    {
-      title: "Design",
-      bodyText: "Old fashioned recipe for preventing allergies and chemical sensitivities.",
-      userIconSources: [UserIcon5, OvalPlusIcon],
-      messageCount: 34,
-      likesCount: 14,
-      attachmentCount: 34,
-      heartColorScheme: "whiteRed",
-      rectangleColors: ['yellow'],
-      showAdditionalText: false,
-    },
-    {
-      showTitle: false,
-      bodyText: "Home business advertising ideas.",
-      userIconSources: [UserIcon6, UserIcon7, OvalPlusIcon],
-      messageCount: 23,
-      heartColorScheme: "whiteRed",
-      likesCount: 12,
-      attachmentCount: 43,
-      rectangleColors: ['yellow', 'red', 'lightPurple', 'lightGreen'],
-      additionalText: "Successful businesses know the importance of building and maintaining good working.",
-    },
-    {
-      showTitle: false,
-      bodyText: "Cosmetic surgery abroad making the right choice.",
-      userIconSources: [UserIcon, UserIcon8, UserIcon9, OvalPlusIcon],
-      messageCount: 54,
-      heartColorScheme: "whiteRed",
-      likesCount: 16,
-      attachmentCount: 33,
-      imageSrc: CardImage1,
-      rectangleColors: ['lightGreen'],
-    }
-  ];
-
-const Cards: React.FC = () => {
+const Cards: React.FC<CardsProps> = ({ cardsData }) => {
     return (
         <StyledCardsWrapper>
             {cardsData.map((cardProps, i) => (
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,14 +3,61 @@ import Navbar from "../../components/Navbar";
 import Brackets from "../../components/Brackets";
 import Cards from "../../components/Cards";
 import { StyledHomeCardsWrapper } from "./HomeStyles";
+import { CardProps } from "../../components/Card";
+import UserIcon5 from "../../assets/images/icons/users/user5.png";
+import OvalPlusIcon from "../../assets/images/icons/users/OvalPlusIcon.png";
+import UserIcon6 from "../../assets/images/icons/users/user6.png";
+import UserIcon7 from "../../assets/images/icons/users/user7.png";
+import CardImage1 from "../../assets/images/CardImage1.png";
+import UserIcon from "../../assets/images/icons/users/user.png";
+import UserIcon8 from "../../assets/images/icons/users/user8.png";
+import UserIcon9 from "../../assets/images/icons/users/user9.png";
 
 const Home: React.FC = () => {
+    const cardsData1: CardProps[] = [
+        {
+            title: "Design",
+            bodyText: "Old fashioned recipe for preventing allergies and chemical sensitivities.",
+            userIconSources: [UserIcon5, OvalPlusIcon],
+            messageCount: 34,
+            likesCount: 14,
+            attachmentCount: 34,
+            heartColorScheme: "whiteRed",
+            rectangleColors: ['yellow'],
+            showAdditionalText: false,
+        },
+        {
+            showTitle: false,
+            bodyText: "Home business advertising ideas.",
+            userIconSources: [UserIcon6, UserIcon7, OvalPlusIcon],
+            messageCount: 23,
+            heartColorScheme: "whiteRed",
+            likesCount: 12,
+            attachmentCount: 43,
+            rectangleColors: ['yellow', 'red', 'lightPurple', 'lightGreen'],
+            additionalText: "Successful businesses know the importance of building and maintaining good working.",
+        },
+        {
+            showTitle: false,
+            bodyText: "Cosmetic surgery abroad making the right choice.",
+            userIconSources: [UserIcon, UserIcon8, UserIcon9, OvalPlusIcon],
+            messageCount: 54,
+            heartColorScheme: "whiteRed",
+            likesCount: 16,
+            attachmentCount: 33,
+            imageSrc: CardImage1,
+            rectangleColors: ['lightGreen'],
+        },
+    ];
+
     return (
         <div>
             <Navbar />
             <Brackets />
             <StyledHomeCardsWrapper>
-                <Cards />
+                <Cards 
+                    cardsData={cardsData1}
+                />
             </StyledHomeCardsWrapper>
         </div>
     );
